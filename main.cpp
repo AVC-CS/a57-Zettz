42b490a5e86ce781c279ad6ef4499bdcc66f5dd7
Lorenzo <calderonlorenzo026@gmail.com>
2026-04-02 18:36:00 +0000

    added a57 code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	int n1, n2;
-	int N, sum;
+    int n1,n2,N;
+    int sum;
+    cin >> n1 >> n2;
+    cin >> N;
+    cout << n1 << " " << n2 << " ";
 
-	cin >> n1 >> n2;
-	cin >> N;
+    for(int i=0; i<N-2; i++)
+    {
+        sum = n1 + n2;
+        cout << sum << " ";
+        n1 = n2;
+        n2 = sum;
 
-	// TODO
-	// TODO
+    }
+    cout << endl;
+    return 0;
 }
